5fd193f373fdf9ff99569ce334ed641376afb4f5
cppascalinux <cppascalinux@gmail.com>
2019-04-11 16:05:35 +0800

    07_0410 b solved

--- 07_0410/b/b1.cpp
+++ 07_0410/b/b1.cpp
@@ -5,6 +5,7 @@
 #include<cstring>
 #include<algorithm>
 #include<cmath>
+#include<ctime>
 #define ll long long
 #define mod 998244353
 #define inv2 499122177
@@ -12,12 +13,11 @@
 using namespace std;
 char *p1,*p2,buffer[10000009];
 int n,k,s,m,len;
-int mul[10],imul[10];
-int v[1000009],id[50009];
+int id[50009];
 int vis[50009],pm[50009],phi[50009];
 int f[50009];
-int t[70009],qt[70009];
-int buc1[70009],buc2[70009],out1[70009],out2[70009];
+int q[70009],nq[70009];
+int buc1[70009],buc2[70009],st1[7000009],st2[7000009];
 void rd(int &x)
 {
 	char ch=0;
@@ -57,9 +57,12 @@ void getphi()
 			phi[i*pm[j]]=phi[i]*(pm[j]-1);
 		}
 	}
+	// for(int i=1;i<=m;i++)
+	// 	printf("i:%d vis:%d phi:%d\n",i,vis[i],phi[i]);
 }
 void fwt(int *s,int c)
 {
+	len=65536;
 	for(int l=2;l<=len;l<<=1)
 	{
 		int d=l>>1;
@@ -77,23 +80,87 @@ void fwt(int *s,int c)
 			}
 	}
 }
-void initm()
+void dfs(int d,int mxd,int sm,int *buc,int *st,int &tp,int mul,int p)
 {
-	mul[0]=1;
-	for(int i=1;i<=k;i++)
-		mul[i]=(ll)mul[i-1]*i%mod;
-	imul[k]=qpow(mul[k],mod-2);
-	for(int i=k-1;i>=0;i--)
-		imul[i]=(ll)imul[i+1]*(i+1)%mod;
-}
-void solve()
-{
-	if(k==1)
-		printf("%lld",(ll)id[s]*s%mod);
-	else if(k==2)
+	if(d>mxd)
 	{
-		
+		if(mul)
+		{
+			buc[sm]=(buc[sm]+mul)%mod;
+			st[++tp]=sm;
+		}
+		return;
 	}
+	for(int i=p;i<=m;i+=p)
+		if(id[i])
+			dfs(d+1,mxd,sm^i,buc,st,tp,(ll)mul*id[i]%mod,p);
+}
+int cal(int nk,int p)
+{
+	int l=sqrt(m);
+	if(p<=80)
+	{
+		memset(q,0,sizeof(q));
+		for(int i=p;i<=m;i+=p)
+			q[i]=id[i];
+		// for(int i=1;i<=m;i++)
+		// 	printf("%d ",q[i]);
+		// printf("\n");
+		// fwt(q,0);
+		// fwt(q,1);
+		// for(int i=1;i<=m;i++)
+		// 	printf("%d ",q[i]);
+		// printf("\n");
+		fwt(q,0);
+		for(int i=0;i<len;i++)
+		{
+			nq[i]=1;
+			for(int j=1;j<=nk;j++)
+				nq[i]=(ll)nq[i]*q[i]%mod;
+		}
+		fwt(nq,1);
+		return nq[s];
+	}
+	int mid=nk>>1,tp1=0,tp2=0;
+	dfs(1,mid,0,buc1,st1,tp1,1,p);
+	dfs(1,nk-mid,0,buc2,st2,tp2,1,p);
+	int ans=0;
+	for(int i=1;i<=tp1;i++)
+	{
+		ans=(ans+(ll)buc1[st1[i]]*buc2[s^st1[i]])%mod;
+		buc1[st1[i]]=0;
+	}
+	for(int i=1;i<=tp2;i++)
+		buc2[st2[i]]=0;
+	return ans;
+}
+int solve(int nk,int p)
+{
+	if(nk==1)
+	{
+		return s%p==0?id[s]:0;
+	}
+	else if(nk==2)
+		return (ll)cal(2,p)*inv2%mod;
+	else if(nk==3)
+	{
+		int now=cal(3,p);
+		int lst=solve(1,p);
+		int sm=0;
+		for(int i=p;i<=m;i+=p)
+			sm+=id[i];
+		now=(now-(ll)(3*sm-2)*lst%mod+mod)%mod;
+		int inv6=qpow(6,mod-2);
+		return (ll)now*inv6%mod;
+	}
+	int now=cal(4,p);
+	int lst=solve(2,p);
+	int sm=0;
+	for(int i=p;i<=m;i+=p)
+		sm+=id[i];
+	now=(now-(ll)(12*sm-16)*lst%mod+mod)%mod;
+	int inv24=qpow(24,mod-2);
+	return (ll)now*inv24%mod;
 }
 int main()
 {
@@ -102,14 +169,17 @@ int main()
 	freopen("b1.out","w",stdout);
 #endif
 	rd(n),rd(k),rd(s);
-	initm();
-	for(int i=1;i<=n;i++)
+	for(int i=1,a;i<=n;i++)
 	{
-		rd(v[i]);
-		m=max(m,v[i]);
-		id[v[i]]++;
+		rd(a);
+		m=max(m,a);
+		id[a]++;
 	}
 	getphi();
-	solve();
+	int ans=0;
+	for(int i=1;i<=m;i++)
+		ans=(ans+(ll)phi[i]*solve(k,i))%mod;
+	printf("%d",ans);
+	cerr<<clock()<<endl;
 	return 0;
 }
--- 07_0410/b/gen.cpp
+++ 07_0410/b/gen.cpp
@@ -9,9 +9,9 @@ int main()
 {
 	freopen("b.in","w",stdout);
 	mt19937 rnd(time(0));
-	int n=1000,k=3,s=500;
+	int n=5000,k=2,s=1000;
 	printf("%d %d %d\n",n,k,s);
 	for(int i=1;i<=n;i++)
-		printf("%d ",rnd()%1000);
+		printf("%d ",rnd()%50000);
 	return 0;
 }
